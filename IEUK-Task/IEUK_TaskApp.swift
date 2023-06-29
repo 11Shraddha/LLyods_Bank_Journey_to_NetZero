7173e56703d89955091427d90a8af7a3d95d8bf7
Chirag <shraddhasojitra11@gmail.com>
2023-06-29 12:33:40 +0100

    Initial Commit

---
+++ IEUK-Task/IEUK_TaskApp.swift
@@ -0,0 +1,20 @@
+//
+//  IEUK_TaskApp.swift
+//  IEUK-Task
+//
+//  Created by Shraddha on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct IEUK_TaskApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
